8988ca826f4e9fce103e07304c0116056db00973
Michael Fine <mfine15@gmail.com>
2013-01-14 18:38:25 -0500

    got playAll working recursively

--- prisoners.hs
+++ prisoners.hs
@@ -12,11 +12,8 @@ grim history
 sucker :: [(Bool,Bool)] -> Bool
 sucker history = True
 
-play :: [(Bool,Bool)] -> ([(Bool,Bool)] -> Bool) -> ([(Bool,Bool)] -> Bool) -> [(Bool,Bool)]
-play history agent1 agent2  = (agent1 history,agent2 history) : history
-
-playAll :: [(Bool,Bool)] -> ([(Bool,Bool)] -> Bool) -> ([(Bool,Bool)] -> Bool) -> [(Bool,Bool)]
-playAll history agent1 agent2 = playAll (play history agent1 agent2) agent1 agent2
+play :: ([(Bool,Bool)] -> Bool) -> ([(Bool,Bool)] -> Bool) -> [(Bool,Bool)] ->  [(Bool,Bool)]
+play a1 a2 history  = (a1 history, a2 history) : play a1 a2 ((a1 history, a2 history):history)
 
 score :: (Bool,Bool) -> (Int,Int)
 score (True,False ) = (0,4)
@@ -27,6 +24,6 @@ score (False,False) = (1,1)
 tally :: [(Bool,Bool)] -> (Int,Int)
 tally history = foldr1 sumTuple (map score history)
 
-
 sumTuple :: (Num a) => (a,a) -> (a,a) -> (a,a)
 sumTuple (a,b) (x,y) = (a+x,b+y)
+
