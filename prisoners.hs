8af698a925936faa18fc1c39e1141a66e498ae19
Michael Fine <mfine15@gmail.com>
2013-01-14 14:12:44 -0500

    switched to tuples

--- prisoners.hs
+++ prisoners.hs
@@ -1,21 +1,21 @@
 
-titForTat :: [Bool] -> Bool
-titForTat history = head history
+titForTat :: [(Bool,Bool)] -> Bool
+titForTat history = fst $ head history
 
-grim :: [Bool] -> Bool
+grim :: [(Bool,Bool)] -> Bool
 grim history
     | null history = True
-    | not (head history) = False
-    | not (head $ tail history) = False
+    | not (fst $ head history) = False
+    | not (snd $ head history) = False
     | otherwise = True
 
-sucker :: [Bool] -> Bool
+sucker :: [(Bool,Bool)] -> Bool
 sucker history = True
 
-play :: [Bool] -> ([Bool] -> Bool) -> ([Bool] -> Bool) -> [Bool]
-play history agent1 agent2  = agent1 history : agent2 history : history
+play :: [(Bool,Bool)] -> ([(Bool,Bool)] -> Bool) -> ([(Bool,Bool)] -> Bool) -> [(Bool,Bool)]
+play history agent1 agent2  = (agent1 history,agent2 history) : history
 
-playAll :: [Bool] -> ([Bool] -> Bool) -> ([Bool] -> Bool) -> [Bool]
+playAll :: [(Bool,Bool)] -> ([(Bool,Bool)] -> Bool) -> ([(Bool,Bool)] -> Bool) -> [(Bool,Bool)]
 playAll history agent1 agent2 = playAll (play history agent1 agent2) agent1 agent2
 
 score :: (Bool,Bool) -> (Int,Int)
@@ -24,12 +24,9 @@ score (False,True ) = (4,0)
 score (True,True  ) = (2,2)
 score (False,False) = (1,1)
 
-tally :: [Bool] -> (Int,Int)
-tally history = foldr1 sumTuple $ map  score $ toTuple history
+tally :: [(Bool,Bool)] -> (Int,Int)
+tally history = foldr1 sumTuple (map score history)
 
-toTuple :: [Bool] -> [(Bool,Bool)]
-toTuple [] = []
-toTuple (x:y:xs) = (x,y): toTuple xs
 
 sumTuple :: (Num a) => (a,a) -> (a,a) -> (a,a)
 sumTuple (a,b) (x,y) = (a+x,b+y)
