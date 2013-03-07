69a3fe948691c4d3e341a905f4b2b2ad5c0dc8e1
Michael Fine <mfine15@gmail.com>
2013-03-07 18:30:33 -0500

    almost working reproduction

--- src/prisoners.hs
+++ src/prisoners.hs
@@ -14,6 +14,7 @@ play a1 a2 history  = (a1 yrotsih, a2 history) : play a1 a2 ((a1 yrotsih, a2 his
 playRound :: [Agent] -> Int -> [Interaction]
 playRound agents iterations = map (makeInteract iterations) (match agents)
 
+
 -- Too long for a lambda function
 makeInteract :: Int -> (Agent,Agent) -> Interaction
 makeInteract iterations (x,y) = Interaction x y (take iterations (play (getFunction x) (getFunction y) []))
@@ -53,24 +54,34 @@ makeAgent (Agent func n _) agents = (Agent func (n++(show $ length $ sameNames n
           findName n1 (Agent _ n2 _) = (slice 0 3 n1) == (slice 0 3 n2) --ignore the suffix
           empty = head $ getEmpty (positions agents) (fst $ getGrid agents) --getGrid returns a tuple, but currently assume to be a square
 
-baseline :: [Interaction] -> Int
-baseline int = foldr (+) (snd $ head $ showSums int) (map snd (showSums int))
+baseline :: [Interaction] -> Float
+baseline int = (fromIntegral total)/len
+  where total = sum sums
+        sums = map snd (showSums int)
+        agents = nub  $ map (\(Interaction a1 a2 _ ) ->  a2) int
+        len  = fromIntegral $ length agents
 
-reproduce :: Int -> [Interaction] -> [Agent]  --so baseline isn't recalulated every time
+reproduce :: Float -> [Interaction] -> [Agent]  --so baseline isn't recalulated every time
 reproduce _ [] = []
-reproduce baseline interaction = winners ++ newAgent:reproduce baseline (tail interaction)
+reproduce base interaction = winners ++ [newAgent]++reproduce base (tail interaction)
     where agents = nub $ concat $ map (\(Interaction a1 a2 _ ) -> a1:a2:[]) interaction
-          winners = filter ((\a -> (sumAgent interaction a) >= baseline )) agents
+          winners = [a | a <- agents, (sumAgent interaction a) >= (round base)]
           newAgent = makeAgent (head winners) winners
 
 
 main = do
+    prefix "Length" (fromIntegral $ length int)
     prefix "Baseline" base
-    prefix "Agents" (generate 16)
+    prefix "Agents" agents
+    prefix "Sums" (showSums int)
+    prefix "winners" winners
+    prefix "NeAgent" (makeAgent (head winners)winners)
     prefix "New Agents" (reproduce base int)
-    where agents = generate 64
+
+    where agents = generate 4
           int = playRound agents 20
           base = baseline int
+          winners = [a | a <- agents, (sumAgent int a) >= (round base)]
 
 
 
